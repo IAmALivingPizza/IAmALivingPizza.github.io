48ef52f0e51c807281cf88d897b0bf6f7722ea6b
IAmALivingPizza <145048724+IAmALivingPizza@users.noreply.github.com>
2023-12-12 11:47:12 -0500

    Add files via upload

---
+++ chapters/chapter_10/apply_10/scripts/script.js
@@ -0,0 +1,13 @@
+/*
+    Student Name: Lucas Rosenberger
+    File Name: script.js
+    Date: December 11, 2023
+*/
+
+//Function to display content
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
