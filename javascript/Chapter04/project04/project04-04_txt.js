e2913490afd88bf58237910230f2c158904ffdc2
IAmALivingPizza <145048724+IAmALivingPizza@users.noreply.github.com>
2024-05-07 14:06:22 -0400

    Add files via upload

--- javascript/Chapter04/project04/project04-04_txt.js
+++ javascript/Chapter04/project04/project04-04_txt.js
@@ -3,20 +3,22 @@
       Project 04-04
 
       Application to determine change from a cash amount
-      Author: 
-      Date:   
+      Author: Lucas Rosenberger
+      Date: May 7, 2024
 
       Filename: project04-04.js
 */
 
 // Global variables
-let cashBox = document.getElementById(cash);
-let billBox = document.getElementById(bill);
-let changeBox = document.getElementById(change);
+let cashBox = document.getElementById("cash");
+let billBox = document.getElementById("bill");
+let changeBox = document.getElementById("change");
+let warningBox = document.getElementById("warning");
 
 // Event handlers to be run when the cash or bill value changes
-cashBox.addEventListener("change", runRegister);
-billBox.addEventListener("change", runRegister);
+cashBox.addEventListener("change", runTheRegister);
+billBox.addEventListener("change", runTheRegister);
+changeBox.addEventListener("change", runTheRegister);
 
 // Function to reset the values in the web page
 function zeroTheRegister() {
@@ -35,12 +37,17 @@ function zeroTheRegister() {
 // Function to run the cash register
 function runTheRegister() {
    zeroTheRegister();
-   
    let changeValue = cashBox.value - billBox.value;  // calculate the change 
-   
-   changeBox.value = formatCurrency(changeValue); // format the change as currency
-   
-   calcChange(changeValue); // Determine the units of currency needed for the change
+   try {
+      if (changeValue < 0)
+         throw "Cash amount doesn't cover the bill";
+      else{
+         changeBox.value = formatCurrency(changeValue); // format the change as currency
+         calcChange(changeValue); // Determine the units of currency needed for the change
+         }
+   } catch (error) {
+      warningBox.innerHTML = error;
+   }
 }
 
 // Function to calculate the change by each unit of currency
@@ -58,7 +65,7 @@ function calcChange(changeValue) {
    // Determine the number of $5 bills
    let bill5Amt = determineCoin(changeValue, 5);
    document.getElementById("bill5").innerHTML = bill5Amt;
-   changeValue -=  bill5Amt*3;  
+   changeValue -=  bill5Amt*5;  
    
    // Determine the number of $1 bills
    let bill1Amt = determineCoin(changeValue, 1);
@@ -85,13 +92,6 @@ function calcChange(changeValue) {
    let coin1Amt = Math.round(changeValue*100);
    document.getElementById("coin1").innerHTML = coin1Amt;
 }
-
-
-
-
-
-
-
 
 /* ================================================================= */
 
